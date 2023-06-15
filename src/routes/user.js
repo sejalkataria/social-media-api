a79a5407fdbc015c233debd2e1d78e190ce27d98
sejalkataria <katariasejal18@gmail.com>
2023-06-15 15:52:12 +0530

    added routes for logout user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
@@ -37,6 +38,25 @@ router.post('/users/login', async (req, res) => {
     catch (e) {
         res.status(400).send({ e: e.message })
     }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    req.user.tokens = []
+    await req.user.save()
+    res.send('Logged out from all devices')
 })
 
 module.exports = router
